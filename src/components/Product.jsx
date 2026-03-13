1072d2515cffa40aff6f4faf73ae47b335d39c46
Parth Thacker <parthathacker@gmail.com>
2026-03-13 17:51:51 +0530

    Added Axios initialiser

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { Spinner, Typography } from "@bigbinary/neetoui";
-import axios from "axios";
+import productsApi from "apis/products";
 import Carousel from "components/Carousel";
 
 const Product = () => {
@@ -10,10 +10,8 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await axios.get(
-        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
-      );
-      setProduct(response.data);
+      const product = await productsApi.show();
+      setProduct(product);
     } catch (e) {
       console.error(e);
     } finally {
@@ -24,7 +22,6 @@ const Product = () => {
   useEffect(() => {
     fetchProduct();
   }, []);
-
   if (isLoading) {
     return (
       <div className="flex h-screen w-full items-center justify-center">
@@ -33,33 +30,32 @@ const Product = () => {
     );
   }
 
-  const discount =
-    product.offer_price &&
-    (((product.mrp - product.offer_price) / product.mrp) * 100).toFixed(2);
+  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  console.log(product);
+  const discount = offerPrice && (((mrp - offerPrice) / mrp) * 100).toFixed(2);
 
-  const imageUrls = product.image_url &&
-    product.image_urls && [product.image_url, ...product.image_urls];
+  const imageUrlsCombined = imageUrl && imageUrls && [imageUrl, ...imageUrls];
 
   return (
     <div className="px-6 pb-6">
       <div>
         <Typography className="py-2 text-4xl font-semibold" style="h1">
-          {product.name}
+          {name}
         </Typography>
         <hr className="border-2 border-black" />
       </div>
       <div className="mt-6 flex gap-4">
         <div className="w-2/5">
           {imageUrls && (
-            <Carousel imageUrls={imageUrls} productName={product.name} />
+            <Carousel imageUrls={imageUrlsCombined} productName={name} />
           )}
         </div>
         <div className="w-3/5 space-y-4">
-          <Typography>{product.description}</Typography>
-          <Typography>MRP: ₹{product.mrp}</Typography>
-          {product.offer_price && (
+          <Typography>{description}</Typography>
+          <Typography>MRP: ₹{mrp}</Typography>
+          {offerPrice && (
             <Typography className="font-semibold">
-              Offer price: ₹{product.offer_price}
+              Offer price: ₹{offerPrice}
             </Typography>
           )}
           {discount && (
